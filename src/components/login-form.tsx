38d7519d73faa63297a34af15fbf5376f0b05dec
assafzvigoren <assafzvigoren4@gmail.com>
2021-04-21 00:56:50 +0300

    add routes and login and register with server

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UserBaseDetails } from '../models/user-base-details'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
 
 interface LoginProps {
   onSubmit: (user: UserBaseDetails) => void,
-  option?: LoginFormOptions
+  option?: LoginFormOptions,
+  errorMessage?: string
 }
 
 export enum LoginFormOptions {
@@ -15,18 +16,27 @@ export enum LoginFormOptions {
 
 export function LoginForm(props: LoginProps) {
   const currOption = props.option ? props.option! : LoginFormOptions.login
+  const [emailValue, setEmailValue] = useState("")
+  const [passwordValue, setPasswordValue] = useState("")
+
+  function clickedSubmit() {
+    props.onSubmit({email: emailValue, password: passwordValue})
+  }
 
   return (
     <div>
-      <form className="" noValidate autoComplete="off">
+      <form className="" autoComplete="off">
         <div>
-          <TextField required id="email" label="email" />
+          <TextField required id="email" value={emailValue} label="email" onChange={e => setEmailValue(e.target.value)} />
         </div>
         <div>
-          <TextField required id="password" type="password" label="password" />
+          <TextField required id="password" type="password" value={passwordValue} onChange={e => setPasswordValue(e.target.value)} label="password" />
         </div>
         <div>
-          <Button variant="contained">{currOption === LoginFormOptions.register ? 'Register' : 'Login'}</Button>
+          <Button variant="contained" onClick={clickedSubmit}>{currOption === LoginFormOptions.register ? 'Register' : 'Login'}</Button>
+        </div>
+        <div>
+          {props.errorMessage !== "" ? props.errorMessage : ""}
         </div>
       </form>
     </div>
--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -6,18 +6,20 @@ import {
   Link
 } from "react-router-dom"
 import Drawer from "@material-ui/core/Drawer"
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import List from '@material-ui/core/List'
 import { LoginForm, LoginFormOptions } from './login-form'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 import ReactLogo from '../logo.svg'
+import { UserBaseDetails } from '../models/user-base-details'
+import axios from 'axios'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -42,67 +44,79 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export function WelcomePage() {
   const [isDrawerDisplayed, setIsDrawerDisplayed] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
   const classes = useStyles()
 
   function toggleDrawer(isOpen: boolean) {
     setIsDrawerDisplayed(isOpen)
   }
+
+  async function login(loginData: UserBaseDetails) { 
+    try {
+      const response = await axios.post('http://localhost:9000/api/users/login', loginData)
+      setErrorMessage("")
+    } catch (err) {
+      // err.status.data
+      // err.status.status
+      setErrorMessage(err.response.data)
+      console.error({...err})
+    }    
+  }
+
+  async function register(loginData: UserBaseDetails) { 
+    try {
+      const response = await axios.post('http://localhost:9000/api/users/register', loginData)
+      setErrorMessage("")
+    } catch (err) {
+      // err.status.data
+      // err.status.status
+      setErrorMessage(err.response.data)
+      console.error({...err})
+    }    
+  }
+
   return (
     <Router>
       <div className={classes.root}>
         <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => setIsDrawerDisplayed(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            <Link className={classes.link} to="/">EMS</Link>
-          </Typography>
-          <Link className={classes.link} to="/login"><Button variant="outlined" color="secondary">Login</Button></Link>
-          <Link className={classes.link} to="/register"><Button variant="outlined" color="secondary">Register</Button></Link>
-        </Toolbar>
-        <div className={classes.list}>
-        <React.Fragment key="drawer">
-        <Drawer
-          anchor="left"
-          open={isDrawerDisplayed}
-          onClose={() => toggleDrawer(false)}
-        >
-          <List>
-          {['Login','Register'].map((text) => (
-            <ListItem button key={text}>
-              <Link to={text}><ListItemText primary={text} onClick={() => toggleDrawer(false)} /></Link>
-            </ListItem>
-          ))}
-          </List>
-        </Drawer>
-      </React.Fragment>
-        </div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-        </nav> */}
-        <Switch>
-          <Route path="/login">
-            <LoginForm onSubmit={() => {}} />
-          </Route>
-          <Route path="/register">
-            <LoginForm onSubmit={() => {}} option={LoginFormOptions.register} />
-          </Route>
-          <Route path="/">
-            lol
-            {/* <img src={ReactLogo} alt="React Logo"/> */}
-          </Route>
-        </Switch>
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={() => setIsDrawerDisplayed(true)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              <Link className={classes.link} to="/">EMS</Link>
+            </Typography>
+            <Link className={classes.link} to="/login"><Button variant="outlined" color="secondary">Login</Button></Link>
+            <Link className={classes.link} to="/register"><Button variant="outlined" color="secondary">Register</Button></Link>
+          </Toolbar>
+          <div className={classes.list}>
+            <React.Fragment key="drawer">
+              <Drawer
+                anchor="left"
+                open={isDrawerDisplayed}
+                onClose={() => toggleDrawer(false)}
+              >
+                <List>
+                  {['Login','Register'].map((text) => (
+                    <ListItem button key={text}>
+                      <Link to={text}><ListItemText primary={text} onClick={() => toggleDrawer(false)} /></Link>
+                    </ListItem>
+                  ))}
+                </List>
+              </Drawer>
+            </React.Fragment>
+          </div>
+          <Switch>
+            <Route path="/login">
+              <LoginForm onSubmit={login} errorMessage={errorMessage} />
+            </Route>
+            <Route path="/register">
+              <LoginForm onSubmit={register} option={LoginFormOptions.register} errorMessage={errorMessage} />
+            </Route>
+            <Route path="/">
+              <img src={ReactLogo} alt="React Logo"/>
+            </Route>
+          </Switch>
         </AppBar>
       </div>
     </Router>

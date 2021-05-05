4615e9b10fae87cf5bcc4309169803a2dbd96426
assafzvigoren <assafzvigoren4@gmail.com>
2021-05-05 19:33:17 +0300

    remove dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import {WelcomePage} from './components/welcome-with-context'
 import {AuthProvider} from './auth/AuthContext'
 import './index.css';
-import {config} from 'dotenv'
-config()
 
 ReactDOM.render(
   <React.StrictMode>

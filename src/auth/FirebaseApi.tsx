8059a9f8c6ef966ce76cf981b91c904a69ff04b3
assafzvigoren <assafzvigoren4@gmail.com>
2021-05-05 18:30:51 +0300

    remove firebase ui

--- src/auth/FirebaseApi.tsx
+++ src/auth/FirebaseApi.tsx
@@ -1,7 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
-import * as firebaseui from 'firebaseui'
 import {firebaseConfig} from './FirebaseConfig'
 
 class FirebaseApi {
